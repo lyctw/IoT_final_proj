07c853000ccdd01b0de35a84ca0d33312c5976ab
syokujinau <npr286rx@rocketmail.com>
2018-12-17 22:41:27 +0800

    reply

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-const LINEBot = require('line-messaging');
+const linebot = require('linebot');
+const express = require('express');
 
-// const CHANNEL_ID           = '1631832658',
-//       CHANNEL_SECRET       = 'acc649e977849b0c56668cf65fb3b78f',
-//       CHANNEL_ACCESS_TOKEN = 'FISyLnxb8ogt53pxKzItlGkw1UMkSfY6FCeExz9CQYKT8nedVVN+8AmqDDq/B0yzHRsTfwyjez9Xf6nzcSNpkucQ6BRwDCB5BXaDS/ZLr014tc9eFMC4x2WVqv2H8Tlj1gpV2K51BVw9Q9yct03smwdB04t89/1O/w1cDnyilFU=';
-
-
-var app = require('express')();
-const server = require('http').Server(app)
-
-var bot = LINEBot.Client({
-  channelID: process.env.CHANNEL_ID,
-  channelSecret: process.env.CHANNEL_SECRET,
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
-}, server);
-
-app.use(bot.webhook('/webhook'));
-
-bot.on(LINEBot.Events.MESSAGE, function(replyToken, message) {
-    bot.replyTextMessage(replyToken, 'hello!').then(function(data) {
-    // add your code when success.
-    }).catch(function(error) {
-    // add your code when error.
-    });
+const bot = linebot({
+	channelId: process.env.CHANNEL_ID,
+	channelSecret: process.env.CHANNEL_SECRET,
+	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 
+const app = express();
 
+const linebotParser = bot.parser();
 
-bot.listen(process.env.PORT);
+app.get('/',function(req,res){
+    res.send('Hello World!');
+});
+
+app.post('/linewebhook', linebotParser);
+
+bot.on('message', function (event) {
+	event.reply(event.message.text).then(function (data) {
+		console.log('Success', data);
+	}).catch(function (error) {
+		console.log('Error', error);
+	});
+});
+
+app.listen(process.env.PORT || 80, function () {
+	console.log('LineBot is running.');
+});
