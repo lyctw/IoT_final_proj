5b989033c35c74401fcb7de7981e517d29510068
syokujinau <npr286rx@rocketmail.com>
2018-12-17 22:25:20 +0800

    reply

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ bot.on('message', function (event) {
     var replyMsg = `Hello你剛才說的是:${event.message.text}`;
     // 透過event.reply(要回傳的訊息)方法將訊息回傳給使用者
     event.reply(replyMsg).then(function (data) {
-        // 當訊息成功回傳後的處理
+        console.log(data);
     }).catch(function (error) {
-        // 當訊息回傳失敗後的處理
+        console.log(error);
     });
 });
 
