c60e1526c2eab7eea8977ed45feb44a2782bdb6c
syokujinau <npr286rx@rocketmail.com>
2018-12-17 22:04:12 +0800

    e

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const linebot = require('linebot');
 const express = require('express');
 
-// const CHANNEL_ID           = '1631087660',
-//       CHANNEL_SECRET       = '3a4cae499590fbc23d2a7fbeaca1e2b5',
-//       CHANNEL_ACCESS_TOKEN = 'uIwge3H5nhPcEyBXdCwGMnJs//9ZKW1iaTMv8VN4xl3hw8k13fN8WqORAd5YpqCmf+HC1ZtS/QAXtwm8RcYLz2FjsIsfctyI3xYD70SMO3K/SuVRVs+vW/NlkQX7gyBrMOr2DGmiw6s44OuUgANLEQdB04t89/1O/w1cDnyilFU=';
+// const CHANNEL_ID           = '1631832658',
+//       CHANNEL_SECRET       = 'acc649e977849b0c56668cf65fb3b78f',
+//       CHANNEL_ACCESS_TOKEN = 'FISyLnxb8ogt53pxKzItlGkw1UMkSfY6FCeExz9CQYKT8nedVVN+8AmqDDq/B0yzHRsTfwyjez9Xf6nzcSNpkucQ6BRwDCB5BXaDS/ZLr014tc9eFMC4x2WVqv2H8Tlj1gpV2K51BVw9Q9yct03smwdB04t89/1O/w1cDnyilFU=';
 
 const bot = linebot({
     channelId: CHANNEL_ID,
